e697ea796fa58e49710f33b500295829204f7cd9
Salman Allai <salmanallaie@gmail.com>
2026-04-01 11:38:31 +0530

    Update CORS with Vercel URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,22 @@ const bookingRoutes = require('./routes/bookingRoutes');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://restaurant-booking-system-inky.vercel.app',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
